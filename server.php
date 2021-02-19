382effabdb3e263ae5c8731ced5a388a019dcea7
Dudi Rudiana <dudirudiana1994@gmail.com>
2021-02-19 08:43:58 +0700

    pertama kali

--- server.php
+++ server.php
@@ -4,7 +4,7 @@
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+ * @author   Dudi Rudiana <dudirudiana1994@gmail.com.com>
  */
 
 $uri = urldecode(
